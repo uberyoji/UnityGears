8e8f789b7214f9e22c8d43c556e5b26a777a9fdf
uberyoji <jimmy.beaubien@gmail.com>
2019-05-12 00:01:58 -0400

    Playing around with gears
    - Fixed issue with mesh filter editor where menu options where not displayed
    - Made materials more metallic
    - Created one more test gear

--- Assets/Editor/NormalsVisualizer.cs
+++ Assets/Editor/NormalsVisualizer.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 [CustomEditor(typeof(MeshFilter))]
 public class NormalsVisualizer : Editor
 {
-    public float NormalScale = 0.1f;
+    public float NormalScale = 0.25f;
+    public bool Show = false;
 
     private Mesh mesh;
 
@@ -17,9 +18,20 @@ public class NormalsVisualizer : Editor
         }
     }
 
+    public override void OnInspectorGUI()
+    {
+        // Show default inspector property editor
+        DrawDefaultInspector();
+
+        if (Show = EditorGUILayout.Toggle("Show Normals", Show))
+        {
+            NormalScale = EditorGUILayout.Slider("Normal Scale", NormalScale, 0.1f, 4f);
+        }
+    }
+
     void OnSceneGUI()
     {
-        if (mesh == null)
+        if (mesh == null || !Show )
         {
             return;
         }
