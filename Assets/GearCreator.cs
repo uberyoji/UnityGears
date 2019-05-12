ee0505f46c45bab06235fda04be5ee350ac89e79
uberyoji <jimmy.beaubien@gmail.com>
2019-05-11 23:24:59 -0400

    Cleaning up!
    - Fixed bug with normal generation on teeth
    - Added tool to debug normals
    - Rewrote teeth generation to support inner teeth
    - Added a grey gear to test

--- Assets/GearCreator.cs
+++ Assets/GearCreator.cs
@@ -11,11 +11,12 @@ using UnityEngine;
 public class GearCreator : MonoBehaviour
 {
     public float InnerRadius = 1f;
+    public float InnerToothDepth = 0.7f;
     public float OuterRadius = 4f;
+    public float OuterToothDepth = 0.7f;
     public float Width = 4f;
     public int TeethCount = 20;
-    public float ToothDepth = 0.7f;
-
+    
     private bool ForceUpdateMesh = false;
 
     void Start()
@@ -40,20 +41,192 @@ public class GearCreator : MonoBehaviour
 
     void BuildMesh()
     {
+        int i = 0;
         CombineInstance[] combine = new CombineInstance[4];
 
-        combine[0].mesh = GetSideFace(true);
-        combine[0].transform = Matrix4x4.identity;
-        combine[1].mesh = GetInnerSurface();
-        combine[1].transform = Matrix4x4.identity;
-        combine[2].mesh = GetOuterSurface();
-        combine[2].transform = Matrix4x4.identity;
-        combine[3].mesh = GetSideFace(false);
-        combine[3].transform = Matrix4x4.identity;
+        combine[i].mesh = GetSideSurface(true);
+        combine[i++].transform = Matrix4x4.identity;
+
+        combine[i].mesh = GetTeethSurface(true);
+        combine[i++].transform = Matrix4x4.identity;
+
+        combine[i].mesh = GetTeethSurface(false);
+        combine[i++].transform = Matrix4x4.identity;
+
+        combine[i].mesh = GetSideSurface(false);
+        combine[i++].transform = Matrix4x4.identity;
 
         GetComponent<MeshFilter>().sharedMesh = new Mesh();
-        GetComponent<MeshFilter>().sharedMesh.CombineMeshes(combine);
+        GetComponent<MeshFilter>().sharedMesh.CombineMeshes(combine);        
     }
+
+    Vector3 GetVector( float r, float a, float w )
+    {
+         return new Vector3(r * Mathf.Cos(a), r * Mathf.Sin(a), w);
+    }
+
+    // Generate gear side face including teeth on outside and inside
+    Mesh GetSideSurface(bool IsFront)
+    {
+        int i, k, bv, bi;
+        float r0, r1, r2, r3;
+        float angle, da;
+
+        r0 = InnerRadius - InnerToothDepth / 2.0f;
+        r1 = InnerRadius + InnerToothDepth / 2.0f;
+        r2 = OuterRadius - OuterToothDepth / 2.0f;
+        r3 = OuterRadius + OuterToothDepth / 2.0f;
+
+        da = 2.0f * Mathf.PI / TeethCount / 4.0f;
+
+        const int VertexCount = 10; // per teeth
+        Vector3[] V = new Vector3[VertexCount * TeethCount];
+        Vector3[] N = new Vector3[VertexCount * TeethCount];
+        for (int n = 0; n < VertexCount * TeethCount; n++)
+        {
+            N[n] = new Vector3(0f, 0f, IsFront ? 1f : -1f);
+        }
+
+        const int FaceCount = 8;
+        int[] I = new int[3 * FaceCount * TeethCount];
+
+        int[,] F = new int[FaceCount, 3] { { 1, 0, 3 }, { 1, 3, 2 }, { 0, 5, 8 }, { 0, 8, 3 }, 
+                                            { 5, 6, 7 },{ 5, 7, 8 },{ 3, 8, 9 },{ 3, 9, 4 } };
+
+        float W = IsFront ? Width : -Width;
+
+        for (i = 0; i < TeethCount; i++)
+        {
+            bv = i * VertexCount;
+            bi = i * FaceCount * 3;
+
+            angle = i * 2.0f * Mathf.PI / TeethCount;
+            V[bv + 0] = GetVector(r1, angle + 0 * da, W * 0.5f);
+            V[bv + 1] = GetVector(r0, angle + 1 * da, W * 0.5f);
+            V[bv + 2] = GetVector(r0, angle + 2 * da, W * 0.5f);
+            V[bv + 3] = GetVector(r1, angle + 3 * da, W * 0.5f);
+            V[bv + 4] = GetVector(r1, angle + 4 * da, W * 0.5f);
+            V[bv + 5] = GetVector(r2, angle + 0 * da, W * 0.5f);
+            V[bv + 6] = GetVector(r3, angle + 1 * da, W * 0.5f);
+            V[bv + 7] = GetVector(r3, angle + 2 * da, W * 0.5f);
+            V[bv + 8] = GetVector(r2, angle + 3 * da, W * 0.5f);
+            V[bv + 9] = GetVector(r2, angle + 4 * da, W * 0.5f);
+
+            k = 0;
+            for (int f = 0; f < FaceCount; f++)
+            {
+                I[bi + k++] = bv + F[f, 0];
+                I[bi + k++] = bv + F[f, IsFront ? 1 : 2];
+                I[bi + k++] = bv + F[f, IsFront ? 2 : 1];
+            }
+        }
+
+        Mesh M = new Mesh
+        {
+            vertices = V,
+            normals = N,
+            triangles = I
+        };
+        return M;
+    }
+
+    // Generate gear faces
+    Mesh GetTeethSurface( bool IsOuter )
+    {
+        int i, k, bv, bi;
+        float r1, r2;
+        float angle, da;
+        float u, v, len;
+
+        if( IsOuter )
+        {
+            r1 = OuterRadius - OuterToothDepth / 2.0f;
+            r2 = OuterRadius + OuterToothDepth / 2.0f;
+        }
+        else
+        {
+            r1 = InnerRadius + InnerToothDepth / 2.0f;
+            r2 = InnerRadius - InnerToothDepth / 2.0f;
+        }        
+
+        da = 2.0f * Mathf.PI / TeethCount / 4.0f;
+
+        const int VertexCount = 16; // per teeth
+        Vector3[] V = new Vector3[VertexCount * TeethCount];
+        Vector3[] N = new Vector3[VertexCount * TeethCount];
+
+        const int FaceCount = 8;
+        int[] I = new int[3 * FaceCount * TeethCount];
+
+        int[,] F = new int[FaceCount, 3] { { 0, 2, 3 }, { 0, 3, 1 }, { 4, 6, 7 }, { 4, 7, 5 }, 
+                                           { 8, 10, 11 }, { 8, 11, 9 }, { 12, 14, 15 }, { 12, 15, 13 } };
+        
+        for (i = 0; i < TeethCount; i++)
+        {
+            bv = i * VertexCount;
+            bi = i * FaceCount * 3;
+            angle = i * 2.0f * Mathf.PI / TeethCount;
+
+            V[bv + 0] = GetVector(r1, angle + 0 * da, Width * -0.5f);
+            V[bv + 1] = GetVector(r1, angle + 0 * da, Width * 0.5f);
+            V[bv + 2] = GetVector(r2, angle + 1 * da, Width * -0.5f);
+            V[bv + 3] = GetVector(r2, angle + 1 * da, Width * 0.5f);
+
+            u = r2 * Mathf.Cos(angle + da) - r1 * Mathf.Cos(angle);
+            v = r2 * Mathf.Sin(angle + da) - r1 * Mathf.Sin(angle);
+            len = Mathf.Sqrt(u * u + v * v) * (IsOuter ? 1f : -1f);
+            u /= len;
+            v /= len;
+            N[bv + 0] = N[bv + 1] = N[bv + 2] = N[bv + 3] = new Vector3(v,-u,0f);
+
+            V[bv + 4] = GetVector(r2, angle + 1 * da, Width * -0.5f);
+            V[bv + 5] = GetVector(r2, angle + 1 * da, Width * 0.5f);
+            V[bv + 6] = GetVector(r2, angle + 2 * da, Width * -0.5f);
+            V[bv + 7] = GetVector(r2, angle + 2 * da, Width * 0.5f);
+
+            N[bv + 4] = N[bv + 5] = N[bv + 6] = N[bv + 7] 
+                = new Vector3( (Mathf.Cos(angle+ 1*da)+Mathf.Cos(angle + 2 * da))/2f, (Mathf.Sin(angle + 1 * da) + Mathf.Sin(angle + 2 * da)) / 2f, 0f) * (IsOuter ? 1f : -1f);
+            
+
+            V[bv + 8] = GetVector(r2, angle + 2 * da, Width * -0.5f);
+            V[bv + 9] = GetVector(r2, angle + 2 * da, Width * 0.5f);
+            V[bv +10] = GetVector(r1, angle + 3 * da, Width * -0.5f);
+            V[bv +11] = GetVector(r1, angle + 3 * da, Width * 0.5f);
+
+            u = r1 * Mathf.Cos(angle + 3 * da) - r2 * Mathf.Cos(angle + 2 * da);
+            v = r1 * Mathf.Sin(angle + 3 * da) - r2 * Mathf.Sin(angle + 2 * da);
+            len = Mathf.Sqrt(u * u + v * v) * (IsOuter ? 1f : -1f);
+            u /= len;
+            v /= len;
+            N[bv + 8] = N[bv + 9] = N[bv + 10] = N[bv + 11] = new Vector3(v, -u, 0f);
+
+            V[bv +12] = GetVector(r1, angle + 3 * da, Width * -0.5f);
+            V[bv +13] = GetVector(r1, angle + 3 * da, Width * 0.5f);
+            V[bv +14] = GetVector(r1, angle + 4 * da, Width * -0.5f);
+            V[bv +15] = GetVector(r1, angle + 4 * da, Width * 0.5f);
+
+            N[bv + 12] = N[bv + 13] = N[bv + 14] = N[bv + 15]
+                = new Vector3((Mathf.Cos(angle + 3 * da) + Mathf.Cos(angle + 4 * da)) / 2f, (Mathf.Sin(angle + 3 * da) + Mathf.Sin(angle + 4 * da)) / 2f, 0f) * (IsOuter ? 1f : -1f);
+
+            // build indice buffer for tooth face
+            k = 0;
+            for (int f = 0; f < FaceCount; f++)
+            {
+                I[bi + k++] = bv + F[f, 0];
+                I[bi + k++] = bv + F[f, IsOuter ? 1 : 2];
+                I[bi + k++] = bv + F[f, IsOuter ? 2 : 1];
+            }
+        }
+        Mesh M = new Mesh
+        {
+            vertices = V,
+            normals = N,
+            triangles = I
+        };
+        return M;
+    }
+
+
 
     /* Face diagram per teeth
 
@@ -69,7 +242,8 @@ public class GearCreator : MonoBehaviour
 
    */
 
-    // Generate gear face including teeth
+    // Generate gear face including outer teeth (no inner teeth)
+    /*
     Mesh GetSideFace( bool IsFront )
     {
         int i,k,bv,bi;
@@ -77,8 +251,8 @@ public class GearCreator : MonoBehaviour
         float angle, da;
 
         r0 = InnerRadius;
-        r1 = OuterRadius - ToothDepth / 2.0f;
-        r2 = OuterRadius + ToothDepth / 2.0f;
+        r1 = OuterRadius - OuterToothDepth / 2.0f;
+        r2 = OuterRadius + OuterToothDepth / 2.0f;
 
         da = 2.0f * Mathf.PI / TeethCount / 4.0f;
 
@@ -103,13 +277,13 @@ public class GearCreator : MonoBehaviour
             bi = i * FaceCount * 3;
 
             angle = i * 2.0f * Mathf.PI / TeethCount;
-            V[bv + 0] = new Vector3(r0 * Mathf.Cos(angle), r0 * Mathf.Sin(angle), W * 0.5f);
-            V[bv + 1] = new Vector3(r1 * Mathf.Cos(angle), r1 * Mathf.Sin(angle), W * 0.5f);
-            V[bv + 2] = new Vector3(r2 * Mathf.Cos(angle + da), r2 * Mathf.Sin(angle + da), W * 0.5f);
-            V[bv + 3] = new Vector3(r2 * Mathf.Cos(angle + 2 * da), r2 * Mathf.Sin(angle + 2 * da), W * 0.5f);
-            V[bv + 4] = new Vector3(r1 * Mathf.Cos(angle + 3 * da), r1 * Mathf.Sin(angle + 3 * da), W * 0.5f);
-            V[bv + 5] = new Vector3(r1 * Mathf.Cos(angle + 4 * da), r1 * Mathf.Sin(angle + 4 * da), W * 0.5f);
-            V[bv + 6] = new Vector3(r0 * Mathf.Cos(angle + 4 * da), r0 * Mathf.Sin(angle + 4 * da), W * 0.5f);
+            V[bv + 0] = GetVector(r0, angle + 0 * da, W * 0.5f);
+            V[bv + 1] = GetVector(r1, angle + 0 * da, W * 0.5f);
+            V[bv + 2] = GetVector(r2, angle + 1 * da, W * 0.5f);
+            V[bv + 3] = GetVector(r2, angle + 2 * da, W * 0.5f);
+            V[bv + 4] = GetVector(r1, angle + 3 * da, W * 0.5f);
+            V[bv + 5] = GetVector(r1, angle + 4 * da, W * 0.5f);
+            V[bv + 6] = GetVector(r0, angle + 4 * da, W * 0.5f);
 
             k = 0;
             for (int f = 0; f < FaceCount; f++)
@@ -129,8 +303,8 @@ public class GearCreator : MonoBehaviour
         return M;
     }
     
-    // Generate inner gear faces
-    Mesh GetInnerSurface()
+    // Generate inner gear faces (cylinder)
+    Mesh GetInnerCylinder()
     {
         int i, k, bv, bi;
         float r0, angle;
@@ -153,16 +327,16 @@ public class GearCreator : MonoBehaviour
 
             // build vertex and normal buffer for tooth face
             angle = i * 2.0f * Mathf.PI / TeethCount;
-            V[bv + 0] = new Vector3(r0 * Mathf.Cos(angle), r0 * Mathf.Sin(angle), -Width * 0.5f);
+            V[bv + 0] = GetVector(r0, angle, -Width * 0.5f);
+            V[bv + 1] = GetVector(r0, angle,  Width * 0.5f);
             N[bv + 0] = new Vector3(-Mathf.Cos(angle), -Mathf.Sin(angle), 0f);
-            V[bv + 1] = new Vector3(r0 * Mathf.Cos(angle), r0 * Mathf.Sin(angle), Width * 0.5f);
             N[bv + 1] = N[bv + 0];
 
             angle = (i + 1) * 2.0f * Mathf.PI / TeethCount;
             
-            V[bv + 2] = new Vector3(r0 * Mathf.Cos(angle), r0 * Mathf.Sin(angle), Width * 0.5f);
+            V[bv + 2] = GetVector(r0, angle,  Width * 0.5f);
+            V[bv + 3] = GetVector(r0, angle, -Width * 0.5f);
             N[bv + 2] = new Vector3(-Mathf.Cos(angle), -Mathf.Sin(angle), 0f);
-            V[bv + 3] = new Vector3(r0 * Mathf.Cos(angle), r0 * Mathf.Sin(angle), -Width * 0.5f);
             N[bv + 3] = N[bv + 2];
 
             // build indice buffer for tooth face
@@ -183,85 +357,5 @@ public class GearCreator : MonoBehaviour
         };
         return M;
     }
-
-    // Generate outer gear faces
-    Mesh GetOuterSurface()
-    {
-        int i, k, bv, bi;
-        float r0, r1, r2;
-        float angle, da;
-        float u, v, len;
-
-        r0 = InnerRadius;
-        r1 = OuterRadius - ToothDepth / 2.0f;
-        r2 = OuterRadius + ToothDepth / 2.0f;
-
-        da = 2.0f * Mathf.PI / TeethCount / 4.0f;
-
-        const int VertexCount = 16; // per teeth
-        Vector3[] V = new Vector3[VertexCount * TeethCount];
-        Vector3[] N = new Vector3[VertexCount * TeethCount];
-
-        const int FaceCount = 8;
-        int[] I = new int[3 * FaceCount * TeethCount];
-
-        int[,] F = new int[FaceCount, 3] { { 0, 2, 3 }, { 0, 3, 1 }, { 4, 6, 7 }, { 4, 7, 5 }, 
-                                           { 8, 10, 11 }, { 8, 11, 9 }, { 12, 14, 15 }, { 12, 15, 13 } };
-        
-        for (i = 0; i < TeethCount; i++)
-        {
-            bv = i * VertexCount;
-            bi = i * FaceCount * 3;
-            angle = i * 2.0f * Mathf.PI / TeethCount;
-
-            V[bv + 0] = new Vector3(r1 * Mathf.Cos(angle + 0 * da), r1 * Mathf.Sin(angle + 0 * da), Width * -0.5f);
-                        V[bv + 1] = new Vector3(r1 * Mathf.Cos(angle + 0 * da), r1 * Mathf.Sin(angle + 0 * da), Width * 0.5f);
-            V[bv + 2] = new Vector3(r2 * Mathf.Cos(angle + 1 * da), r2 * Mathf.Sin(angle + 1 * da), Width * -0.5f);
-            V[bv + 3] = new Vector3(r2 * Mathf.Cos(angle + 1 * da), r2 * Mathf.Sin(angle + 1 * da), Width * 0.5f);
-
-            u = r2 * Mathf.Cos(angle + da) - r1 * Mathf.Cos(angle);
-            v = r2 * Mathf.Sin(angle + da) - r1 * Mathf.Sin(angle);
-            len = Mathf.Sqrt(u * u + v * v);
-            u /= len;
-            v /= len;
-            N[bv + 0] = N[bv + 1] = N[bv + 2] = N[bv + 3] = new Vector3(v,-u,0f);
-
-            V[bv + 4] = new Vector3(r2 * Mathf.Cos(angle + 1 * da), r2 * Mathf.Sin(angle + 1 * da), Width * -0.5f);
-            V[bv + 5] = new Vector3(r2 * Mathf.Cos(angle + 1 * da), r2 * Mathf.Sin(angle + 1 * da), Width * 0.5f);
-            V[bv + 6] = new Vector3(r2 * Mathf.Cos(angle + 2 * da), r2 * Mathf.Sin(angle + 2 * da), Width * -0.5f);
-            V[bv + 7] = new Vector3(r2 * Mathf.Cos(angle + 2 * da), r2 * Mathf.Sin(angle + 2 * da), Width * 0.5f);
-
-            N[bv + 4] = N[bv + 5] = N[bv + 6] = N[bv + 7] = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0f);
-
-            V[bv + 8] = new Vector3(r2 * Mathf.Cos(angle + 2 * da), r2 * Mathf.Sin(angle + 2 * da), Width * -0.5f);
-            V[bv + 9] = new Vector3(r2 * Mathf.Cos(angle + 2 * da), r2 * Mathf.Sin(angle + 2 * da), Width * 0.5f);
-            V[bv +10] = new Vector3(r1 * Mathf.Cos(angle + 3 * da), r1 * Mathf.Sin(angle + 3 * da), Width * -0.5f);
-            V[bv +11] = new Vector3(r1 * Mathf.Cos(angle + 3 * da), r1 * Mathf.Sin(angle + 3 * da), Width * 0.5f);
-
-            N[bv + 8] = N[bv + 9] = N[bv + 10] = N[bv + 11] = new Vector3(v, -u, 0f);
-
-            V[bv +12] = new Vector3(r1 * Mathf.Cos(angle + 3 * da), r1 * Mathf.Sin(angle + 3 * da), Width * -0.5f);
-            V[bv +13] = new Vector3(r1 * Mathf.Cos(angle + 3 * da), r1 * Mathf.Sin(angle + 3 * da), Width * 0.5f);
-            V[bv +14] = new Vector3(r1 * Mathf.Cos(angle + 4 * da), r1 * Mathf.Sin(angle + 4 * da), Width * -0.5f);
-            V[bv +15] = new Vector3(r1 * Mathf.Cos(angle + 4 * da), r1 * Mathf.Sin(angle + 4 * da), Width * 0.5f);
-
-            N[bv + 12] = N[bv + 13] = N[bv + 14] = N[bv + 15] = N[bv + 4];
-
-            // build indice buffer for tooth face
-            k = 0;
-            for (int f = 0; f < FaceCount; f++)
-            {
-                I[bi + k++] = bv + F[f, 0];
-                I[bi + k++] = bv + F[f, 1];
-                I[bi + k++] = bv + F[f, 2];
-            }
-        }
-        Mesh M = new Mesh
-        {
-            vertices = V,
-            normals = N,
-            triangles = I
-        };
-        return M;
-    }
+    */
 }
